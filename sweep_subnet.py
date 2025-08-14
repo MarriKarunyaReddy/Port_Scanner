5bc445aafc5694bf6abdc9ec0499be4adb7b4ccb
MarriKarunyaReddy <marrikarunyareddy@gmail.com>
2025-08-14 10:18:36 +0530

    add gitignore

--- sweep_subnet.py
+++ sweep_subnet.py
@@ -1,6 +1,6 @@
 import subprocess  #module to run system commands
 import platform    #module to detect host os
-from get_subnet import get_local_ip, get_subnet
+
 from pythonping import ping
 
 def ping_host(host):
@@ -8,18 +8,3 @@ def ping_host(host):
     return response.success()
 
 
-if __name__ == "__main__":
-    local_ip = get_local_ip()
-    print("Local IP: ",local_ip)
-
-    subnet = get_subnet(local_ip)
-    print("Subnet: ", subnet)
-
-    live_hosts= []
-    for i in range(1,255):
-        ip = subnet + str(i)
-        if ping_host(ip):
-            print(f"{ip} is alive")
-            live_hosts.append(ip)
-    
-    print("Live Hosts: ", live_hosts)
